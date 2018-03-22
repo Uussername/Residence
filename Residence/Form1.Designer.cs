24b8395203a6e9158c5d3edbb915d43433567882
AJ Thompson <thoantj@dunwoody.edu>
2018-03-22 09:50:46 -0500

    changed a button font size

--- Residence/Form1.Designer.cs
+++ Residence/Form1.Designer.cs
@@ -227,7 +227,7 @@
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(64, 142);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(176, 51);
@@ -238,7 +238,7 @@
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(64, 72);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(176, 51);
